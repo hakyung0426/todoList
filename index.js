1c56f8a53d7d3005fc8cc4b6213b4cbf2ac2120b
hakyung0426 <kkha0426@naver.com>
2022-06-23 23:08:58 +0900

    get과 post

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require('express')
 var app = express()
 
+//데이터를 POST 방식으로 전송가능
+const bodyParser = require('body-parser')
+//url인코딩을 계속 적용할지
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', function (req, res) {
-res.sendFile(__dirname + '/main.html');
+  res.sendFile(__dirname + '/main.html');
+})
+
+app.get('/login', (req, res)=> {
+  res.send("get");
+})
+
+app.post('/login', (req, res)=> {
+  res.send("post");
 })
 
 app.listen(3000);
