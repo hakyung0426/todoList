587ee09fbf4d1c377163151e4fa5330351697122
hakyung0426 <kkha0426@naver.com>
2022-06-24 23:51:47 +0900

    몽고DB 연결

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express')
 var app = express()
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/todolist';
+var db;
+
+MongoClient.connect(url, function (err, database) {
+   if (err) {
+      console.error('MongoDB 연결 실패', err);
+      return;
+   }
+
+   db = database;
+});
+
 //데이터를 POST 방식으로 전송가능
 const bodyParser = require('body-parser')
 //url인코딩을 계속 적용할지
